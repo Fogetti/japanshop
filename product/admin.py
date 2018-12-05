a3a41a6e1dd8003fda306de4deb267e8d2475413
fogetti <fogetti@gmail.com>
2018-12-05 20:43:46 +0900

    add new image widget to the product model admin

--- product/admin.py
+++ product/admin.py
@@ -1,4 +1,7 @@
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.translation import ugettext_lazy as _
 from django.forms import TextInput, Textarea
+from django.utils.html import format_html
 from django.contrib import admin
 from django.db import models
 
@@ -9,6 +12,18 @@ from product.models import ProductCategory
 from product.models import JapaneseAddress
 from product.models import HungarianAddress
 from product.models import ProductStorageLocation
+
+class AdminImageWidget(AdminFileWidget):
+  def render(self, name, value, attrs=None, renderer=None, **_kwargs):
+    output = []
+    if value and getattr(value, "url", None):
+      image_url = value.url
+      file_name = str(value)
+      output.append(u' <a href="%s" target="_blank"><img src="%s" alt="%s" style="max-width: 100&#37;;height: auto;"/></a> %s ' %
+                    (image_url, image_url, file_name, _('Change:')))
+    output.append(super().render(name, value, attrs))
+    return format_html(u''.join(output))
+
 
 # Register your models here.
 class ProductPriceInline(admin.TabularInline):
@@ -28,6 +43,7 @@ class ProductAdmin(admin.ModelAdmin):
   formfield_overrides = {
       models.CharField: {'widget': TextInput(attrs={'size':'20'})},
       models.TextField: {'widget': Textarea(attrs={'rows':3, 'cols':60})},
+      models.ImageField: {'widget': AdminImageWidget},
   }
 
 
